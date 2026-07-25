7c55dffdc29932d52d8a4cc60d00130106ec1c11
Edghar Danuwijaya <email_anda@example.com>
2026-07-25 09:00:07 +0700

    update

--- app/api/download/route.ts
+++ app/api/download/route.ts
@@ -1,5 +1,6 @@
 import { NextResponse } from 'next/server';
 
+export const dynamic = 'force-dynamic';
 export async function GET(request: Request) {
   try {
     const { searchParams } = new URL(request.url);
